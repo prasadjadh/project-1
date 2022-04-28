a2446551aa8bd26b8549089017522cc0a54ae305
Duttji1996 <prakashdutttripathi@gmail.com>
2022-04-28 18:09:48 +0530

    no changes

--- SRC/controller/authentication.js
+++ SRC/controller/authentication.js
@@ -1,3 +1,11 @@
 const AuthorModel= require('../model/author')
 const BloggerModel= require('../model/blog')
-const JWT =
+const jwt = require("jsonwebtoken");
+
+const Authorization= async function(req,res){
+    let body= req.body;
+    let email= body.email
+    let password= body.email
+
+    let Authorization= await 
+}
